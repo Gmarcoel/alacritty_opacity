7d7dae884dd5f361812a08effcf4600c75f79a7b
Guille <guillermo.martincoello@gmail.com>
2021-11-26 14:51:13 +0100

    Script finished

---
+++ ao.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+
+n=$( grep -n background_opacity ~/.alacritty.yml | awk '{print $2}')
+
+if [ $1 == 'u' ]
+then
+	echo "Script to change alacritty terminal opacity"
+	echo "only one argument:"
+	echo "	u: increase 0.1 opacity"
+	echo "	d: decrease 0.1 opacity"
+	echo "	n: any number from 0 to 1 to give a specific opacity"
+	echo "		ex: 0.7"
+elif [ $1 == 'u' ]
+then
+	new=$(echo $n 0.1 | awk '{print $1 + $2}')
+	if [[ (( $new > '1.0' )) ]]
+	then
+	new=1
+	fi
+elif [ $1 == 'd' ]
+then
+	new=$(echo $n 0.1 | awk '{print $1 - $2}')
+	if [[ (( $new == '-0.1' )) ]]
+	then
+		new=0
+	fi
+elif [[ (( $1 < '1.0' )) ]]
+then
+	new=$1
+elif [[ (( $1 == '1' )) ]]
+then
+	new=$1
+else
+	echo "need an argument: u for increase opacity d to decrease number 0-1.0 to specific opacity"
+	new=$n
+fi
+
+
+sed "s/background_opacity:\s$n/background_opacity: $new/" -i ~/.alacritty.yml
